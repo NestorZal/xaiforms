ba5366bd27aaba456e67fbf3937cb80e970bfac4
Nestor Zaldivar <nzaldivarc@gmail.com>
2023-10-03 15:09:03 -0400

    Add option to pass custom onclick functions

--- src/Includes/js/form/fields/Button.js
+++ src/Includes/js/form/fields/Button.js
@@ -4,7 +4,18 @@ const Button = (props) => {
   const ButtonComponent = "button";
 
   return (
-    <ButtonComponent type={type} className={className} {...rest}>
+    <ButtonComponent
+        type={type}
+        className={className}
+        {...(rest['data-onclick']
+            ? {
+              onClick: () => {
+                window[rest['data-onclick']]();
+              },
+            }
+            : "")}
+        {...rest}
+    >
       {children}
     </ButtonComponent>
   );
