a28ba05d4fce5e005d6f6254f88681df51bbbc41
Nestor Zaldivar <nzaldivarc@gmail.com>
2023-11-08 14:50:32 -0500

    Test payment form

--- src/templates/default.payment.form.php
+++ src/templates/default.payment.form.php
@@ -16,10 +16,21 @@ $fluidpay_route = new \XaiForms\Gateway\FluidPay\FluidPayRoute();
 
     <input name="billing_address[billing_name]" placeholder="Credit Card Holder" required  wrapper-class="billing-name" label="Name on credit card" error-msg-required="Your Name is required." />
 
-    <cardnumber name="payment_method[card][number]" wrapper-class="form-group row" label="Credit card number" class="form-control lock-icon" error-msg-required="Please enter your credit card."/>
-    <expirydate name="payment_method[card][expiration_date]" label="Epiry date" error-msg-invalid="Please try again and insert a valid expiration date."/>
-    <cvc name="payment_method[card][cvc]" label="CVC (3 or 4 digit code)"/>
-    <price name="amount" required error-msg-required="The amount to pay is required." value="145" />
+    <card type="number" name="payment_method[card][number]" aria-label="Card number" autocomplete="cc-number" placeholder="Credit card number here"
+          class="form-control lock-icon" wrapper-class="col-lg-8 col-xl-6"  label="Credit card number"
+          error-msg-required="Please write your credit card number" error-msg-invalid="Please enter a valid credit card number"
+    />
+
+
+    <card type="expiry" label="Expiry date" name="payment_method[card][expiration_date]" class="form-control" wrapper-class="col-sm-6 col-lg-4 col-xl-3"
+          error-msg-required="Please insert an expiry date" error-msg-invalid="Please insert a valid expiry date"
+    />
+
+    <card type="cvc" label="CVC (3 or 4 digit code)" name="payment_method[card][cvc]" class="form-control" wrapper-class="col-sm-6 col-lg-4 col-xl-3"
+          error-msg-required="Please write the CVC number" error-msg-invalid="CVC number must have 3 digits long at least"
+    />
+
+    <input type="price" name="amount" class="form-control" wrapper-class="col-sm-6 col-lg-4 col-xl-3" required placeholder-color="#1275db" error-msg-required="The amount to pay is required."/>
 
     <button type="submit">Pay now!</button>
 
